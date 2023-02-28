74387722b1c3f262c6cc939632d2f9a780b8014f
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-02-28 11:15:02 +0100

    Added ENTER and HEX Checks

---
+++ js/formCheck.js
@@ -0,0 +1,21 @@
+function formCheck(event) {
+  event.preventDefault();
+  var formField = document.getElementById("hex-input");
+  var hex = formField.value;
+  
+  // Check if input is exactly 6 hexadecimal characters
+  if (!/^[0-9a-fA-F]{6}$/.test(hex)) {
+    event.preventDefault();
+    alert("Please enter a valid 6-digit hexadecimal color code");
+    return;
+  }
+
+  // Check if input only contains 0-9 and a-f (both lowercase and uppercase)
+  if (!/^[0-9a-fA-F]*$/.test(hex)) {
+    event.preventDefault();
+    alert("Please only use characters 0-9 and a-f (or A-F) for the color code");
+    return;
+  }
+  
+  setColor();
+}
