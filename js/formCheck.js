1f6e613ae7b8b0732eba8ce2de6855caee78e923
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-01 09:57:33 +0100

    Fixed formCheck

--- js/formCheck.js
+++ js/formCheck.js
@@ -1,16 +1,14 @@
-function formCheck(event) {
+function formCheck() {
   var hex = document.getElementById("hex-input").value;
-  // Check if input is exactly 6 hexadecimal characters
-  if (!/^[0-9a-fA-F]{6}$/.test(hex)) {
-    event.preventDefault();
-    alert("Please enter a valid 6-digit hexadecimal color code");
-    return;
-  }
-
   // Check if input only contains 0-9 and a-f (both lowercase and uppercase)
   if (!/^[0-9a-fA-F]*$/.test(hex)) {
-    event.preventDefault();
     alert("Please only use characters 0-9 and a-f (or A-F) for the color code");
+    return;
+  }
+  
+  // Check if input is exactly 6 hexadecimal characters
+  if (!/^[0-9a-fA-F]{6}$/.test(hex)) {
+    alert("Please enter a valid 6-digit hexadecimal color code");
     return;
   }
   
