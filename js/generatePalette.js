413a0b985da2603dc329ad5e9f057ced33753115
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-04 17:32:52 +0100

    Update generatePalette.js

--- js/generatePalette.js
+++ js/generatePalette.js
@@ -1,5 +1,5 @@
 function getDefault(defColor, defBrightness) {
-    const defaultBoxID = `color${defBrightness}`;
+    const defaultBoxID = `color` + defBrightness;
     const defaultBox = document.getElementById(defaultBoxID);
     defaultBox.classList.add("default");
     defaultBox.style.backgroundColor = defColor;
