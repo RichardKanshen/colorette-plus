0ce448d15e25593cf72dd96fe73db78bbb29291f
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-04 18:31:55 +0100

    Revert "Tried to fix getDefault"
    This reverts commit 1a2d9b5c112a6194f2e1fea07a1f43e29a5a99fc.

--- js/generatePalette.js
+++ js/generatePalette.js
@@ -1,6 +1,5 @@
 function getDefault(defColor, defBrightness) {
-    const defaultBrightness = defBrightness;
-    const defaultBoxID = "color" + defaultBrightness;
+    const defaultBoxID = "color" + defBrightness;
     const defaultBox = document.getElementById(defaultBoxID);
     defaultBox.classList.add("default");
     defaultBox.style.backgroundColor = defColor;
