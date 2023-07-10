88e6f08e700cab830beed4191dc12f8d48be0ba2
Richard Kužela <richard@kanshen.click>
2023-07-10 16:30:49 +0200

    Fixed a bug from v2.2

--- js/checkColor.js
+++ js/checkColor.js
@@ -4,7 +4,6 @@ function checkColor() {
   const random = urlParams.get('random');
   if (color) {
     document.getElementById('hex-input').value = color;
-    changeTheme(color);
     document.getElementById('color0').style.display = "flex";
     document.getElementById('color1').style.display = "flex";
     document.getElementById('color2').style.display = "flex";
@@ -21,10 +20,12 @@ function checkColor() {
     document.querySelector('title').textContent = "#" + color + " | colorétte +"
     
     if (color.length === 6) {
+    changeTheme(color);
     getColorBrightness(color);
     checkContrast(color)
     } else if (color.length === 3) {
       var formattedcolor = `${color[0]}${color[0]}${color[1]}${color[1]}${color[2]}${color[2]}`;
+      changeTheme(formattedcolorcolor);
       getColorBrightness(formattedcolor);
       checkContrast(formattedcolor)
     }
